fe79c283353039ecf97ba3f23052cfc69b3d8bb5
sanjay-dev-patidar <142602180+sanjay-dev-patidar@users.noreply.github.com>
2023-08-23 15:27:44 +0530

    Update server.cjs

--- backend/server.cjs
+++ backend/server.cjs
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
 const Query = require('./queryModel.js');
+require('dotenv').config();
+
 const app = express();
 app.use(cors());
 
